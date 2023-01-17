73aaaabe290a80e7355abc0d61625723cbfeef64
Cheah Thean Sze <thean-sze.cheah6@keysight.com>
2023-01-17 16:59:34 +0800

    I am happy again

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "I hate the World!";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
